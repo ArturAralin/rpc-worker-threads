ce55c86bb038eb6f3939c587990793a16b0b09a3
Artur Aralin <artur.aralin97@gmail.com>
2020-01-06 20:22:56 +0300

    change main file

--- src/rpc-worker-thread.ts
+++ src/rpc-worker-thread.ts
@@ -1,11 +1,89 @@
-import { RPCWorker } from './rpc';
+import {
+  Worker,
+  WorkerOptions,
+  MessageChannel,
+  MessagePort,
+} from 'worker_threads';
+import * as path from 'path';
+import { v4 as genUUID } from 'uuid';
+import {
+  RPCRequest,
+  RPCResponse,
+} from './interfaces';
 
-(async () => {
-  const worker = new RPCWorker('some file');
+const WORKER_INIT_FILE = path.resolve(__dirname, './init-file.js');
 
-  await worker.init();
+interface TaskFNs {
+  resolve: (v: any) => void;
+  reject: (v: any) => void;
+}
 
-  const r = await worker.send('test_task', { a: 10, b: 20 });
+export class RPCWorker extends Worker {
+  private started = false;
+  private msgPort!: MessagePort;
+  private tasks: Map<string, TaskFNs> = new Map();
 
-  console.log('finished', r);
-})()
+  constructor(filename: string, options?: WorkerOptions | undefined) {
+    super(WORKER_INIT_FILE, options);
+  }
+
+  private responseHandler(res: RPCResponse) {
+    const task = this.tasks.get(res.taskId);
+
+    if (!task) {
+      throw new Error(`Unknown task id ${res.taskId}`);
+    }
+
+    if (res.status === 'success') {
+      task.resolve(res.response);
+
+      return;
+    }
+
+    task.reject({
+      status: res.status,
+      response: res.response,
+    });
+  }
+
+  init() {
+    return new Promise((resolve, reject) => {
+      const { port1, port2 } = new MessageChannel();
+
+      this.msgPort = port2;
+
+      this.once('message', (msg) => {
+        if (msg === 'ready') {
+          this.started = true;
+          this.msgPort.on('message', this.responseHandler.bind(this));
+
+          resolve();
+
+          return;
+        }
+
+        reject();
+      });
+
+      this.postMessage(port1, [port1]);
+    });
+  }
+
+  send(name: string, data?: any) {
+    if (!this.started) {
+      throw new Error('RPC Worker is not started');
+    }
+
+    const taskId = genUUID();
+
+    return new Promise((resolve, reject) => {
+      this.tasks.set(taskId, { resolve, reject });
+
+      this.msgPort.postMessage({
+        taskId,
+        name,
+        data,
+      } as RPCRequest);
+    });
+  }
+}
